3ca3cb23e0910128936773df589b97280bc52754
Johannes Schmidt <devshred@tigerflanke.de>
2022-11-23 21:16:14 +0100

    align table name with other implementations

--- src/main/java/todo/TodoEntity.java
+++ src/main/java/todo/TodoEntity.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -15,6 +16,7 @@ import todo.model.TodoItem;
 @Entity
 @Getter
 @NoArgsConstructor
+@Table(name = "todos")
 public class TodoEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
