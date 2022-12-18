cbd5cbe5e65341c30bd157ab9353df312f165307
Johannes Schmidt <devshred@tigerflanke.de>
2022-12-18 16:02:01 +0100

    remove pushing to local registry

--- build.sh
+++ build.sh
@@ -9,13 +9,9 @@ help() {
 if [ "$mode" == "native" ]; then
    ./mvnw -Pnative -DskipTests clean native:compile
   docker build -f src/main/docker/Dockerfile.native -t todo-api-spring-java:native .
-  docker tag todo-api-spring-java:native registry.local:5000/todo-api-spring-java:native
-  docker push registry.local:5000/todo-api-spring-java:native
 elif [ "$mode" == "jvm" ]; then
   ./mvnw clean package -DskipTests
   docker build -f src/main/docker/Dockerfile.jvm -t todo-api-spring-java:jvm .
-  docker tag todo-api-spring-java:jvm registry.local:5000/todo-api-spring-java:jvm
-  docker push registry.local:5000/todo-api-spring-java:jvm
 elif [ -z "$mode" ]; then
   echo "mode not set"
   help
